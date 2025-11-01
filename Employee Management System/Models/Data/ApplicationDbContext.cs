49129af44280ccc5501fba72058c86caf185e1ed
Amr Talaat <amrt2598@gmail.com>
2025-11-01 21:33:22 +0200

    Add project files.

---
+++ Employee Management System/Models/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Employee_Management_System.Models;
+
+namespace Employee_Management_System.Moduls.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Employee Management System/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace Employee_Management_System.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public string Position { get; set; }
+    public string Department { get; set; }
+    public decimal Salary { get; set; }
+    public DateTime HireDate { get; set; }
+
+}
